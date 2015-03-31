401a18218a4c13015353a7b390d2d65ef50e8af6
JRoffel <jasonroffel@hotmail.nl>
2015-03-31 12:21:52 +0200

    Version 0.0.5
    Added SQL, PHP and started working on functionality!

--- index.action.php
+++ index.action.php
@@ -1,2 +1,5 @@
 <?php
-	$connection = new mysqli("localhost", "root", "", "people")
+	$connection = new mysqli("localhost", "root", "", "people");
+	$query = 'select * from person';
+	$result = $connection -> query($query);
+	$persons = $result -> fetch_all(MYSQLI_ASSOC);
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require'index.action.php' ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,7 +8,16 @@
 
 	<body>
 		<div class="master">
-			
+			<div class="content">
+				<h1>Personen</h1>
+				<?php foreach($persons as $person):?>
+				<p><?= $person['id']?>  <?= $person['name'] ?>  <?= $person['date_of_birth']?> </p>
+				<?php endforeach; ?>
+			</div>
+
+			<div class="footer">
+				<p>This page is made by Jason (10/10 would recreate)</p>
+			</div>
 		</div>
 	</body>
 </html>
