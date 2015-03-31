adca381ef62a187a7b3c18e74e3975e42e1af797
JRoffel <jasonroffel@hotmail.nl>
2015-03-31 11:40:51 +0200

    Version basic
    Upping basics (Like always)

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Querysort</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<div class="master">
+			
+		</div>
+	</body>
+</html>
